21fcb4f53ed371f000c1fffe85f807a863903129
zelaskov <marcin.zelasko@codetain.eu>
2021-08-17 11:16:22 +0200

    change goodbye script for test purposes

--- goodbye.sh
+++ goodbye.sh
@@ -1,1 +1,5 @@
-echo "Goodbye"
+sudo apt-get install update -y
+sudo apt-get install git -y
+sudo apt-get install gpg -y
+wget https://packages.chef.io/files/stable/chef-workstation/21.7.524/ubuntu/20.04/chef-workstation_21.7.524-1_amd64.deb
+sudo dpkg -i chef-workstation_21.7.524-1_amd64.deb
